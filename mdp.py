526056f0f639e20844d26728b22c4e3e8e88039e
Nils Martel <nils.martel@graphmasters.net>
2020-09-23 14:19:48 +0200

    Fix Value iteration

--- mdp.py
+++ mdp.py
@@ -1,34 +1,5 @@
 import math
 
-class MDP:
-    # states: [States]
-    # actions: [Action]
-    # transitions: (State, Action) -> [(Number, State)]
-    # utility: (State, Action) -> Number
-    def __init__(states, actions, transitions, utility):
-        self.states = states
-        self.actions = actions
-        self.t = transitions
-        self.u = utility
-    
-    def heuristic(self, iterations):
-        if iterations == 0:
-            return emptyHeuristc(self.states, self.action)
-
-        heuristic = self.heuristic(iterations - 1)
-
-        newHeuristic = emptyHeuristic(self.states, self.actions)
-        for s in self.states:
-            for a in self.actions:
-                value = 0.0
-                for (p, s) in self.t(s, a):
-                    value += p * maxValue(heuristic[s])
-
-                newHeuristic[s][a] = value
-
-        return newHeuristic
-                
-
 def maxValue(d):
     n = -math.inf
     for (k, v) in d:
@@ -44,7 +15,38 @@ def emptyHeuristic(states, actions):
         d = dict()
         for a in actions:
             d[a] = 0.0
-        
+
         policy[s] = d
-    
+
     return policy
+
+class MDP:
+    # states: [States]
+    # actions: [Action]
+    # transitions: (State, Action) -> [(Number, State)]
+    # reward: (State, Action, State') -> Number
+    def __init__(self, states, actions, transitions, reward, falloff = 1.0):
+        self.states = states
+        self.actions = actions
+        self.t = transitions
+        self.reward = reward
+        self.falloff = falloff
+
+
+    def heuristic(self, iterations):
+        if iterations == 0:
+            return emptyHeuristic(self.states, self.actions)
+
+        heuristic = self.heuristic(iterations - 1)
+
+        newHeuristic = emptyHeuristic(self.states, self.actions)
+
+        for s in self.states:
+            for a in self.actions:
+                value = 0.0
+                for (p, sn) in self.t(s, a):
+                    value += p* (self.reward(s, a, sn) + self.falloff*maxValue(heuristic[sn]))
+
+                newHeuristic[s][a] = value
+
+        return newHeuristic
